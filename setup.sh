47a3d2c6085e3f53625dfcdf47cd508411b1776e
diwakar <83821864+diwusec@users.noreply.github.com>
2021-10-09 00:15:26 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "installing docker"
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+echo "installing lazydocker"
+curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+echo "done"
