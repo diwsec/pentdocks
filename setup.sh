d1333afefbb852b0b1c85a2c8a139f93d2b1ab81
diwakar <83821864+diwusec@users.noreply.github.com>
2022-02-24 11:54:52 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 echo "installing lazydocker"
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
-sudo mv lazydocker ~/usr/local/bin
+sudo mv lazydocker /usr/local/bin
 echo "done"
